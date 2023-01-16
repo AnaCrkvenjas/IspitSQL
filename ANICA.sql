69926d31ab9947e96d1fac2aa77c7e32cc91fa90
AnaCrkvenjas <81113000+AnaCrkvenjas@users.noreply.github.com>
2023-01-16 16:04:59 +0100

    Prvi zadatak

---
+++ ANICA.sql
@@ -0,0 +1,4 @@
+select * from T_Document
+where Meta_String6='07031004447679' and sysDeleted='N'
+
+select Meta_Long4,* from [PUFBIH_NPIS].[dbo].[T_Document] where Meta_String6='08041003387094' and sysDeleted='N'
